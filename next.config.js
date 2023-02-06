314f08436aec060c35f2434b76d0d26ed592901a
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-02-06 11:35:06 -0300

    add stories e config pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
-/** @type {import('next').NextConfig} */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   compiler: {
     styledComponents: true
   }
-}
+})
 
-module.exports = nextConfig
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true
+//   }
+// }
+
+// module.exports = nextConfig
